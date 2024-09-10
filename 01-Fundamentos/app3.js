173196eb24c9a442bb1742fc76558d641b652c3e
soy eddy <soyeddy.email.com>
2024-09-09 22:24:23 -0600

    contador de palabras usando expresion regular y case insensitive para no distinguir de mayusculas de minusculas

---
+++ 01-Fundamentos/app3.js
@@ -0,0 +1,30 @@
+
+//Tarea contador de palabras
+const fs = require('fs');
+
+const content = fs.readFileSync('README.md', 'utf-8');
+
+const wordCount = content.split('').length
+
+const reactWordCount = content.match(/React/gi ?? []).length//si no encuentra ninguna palabra devuelve un array vacio y se le pone un length para que devuelva 0
+
+//content.match(/React/ig) devuelve un array con todas las palabras que coinciden con la expresion regular
+//las expresiones regulares se usan para buscar patrones en strings
+//ig significa que la busqueda es case insensitive y global 
+//insensitive significa que no distingue entre mayusculas y minusculas
+
+
+//match es un metodo de los strings que devuelve un array con todas las coincidencias de la expresion regular
+//ejem de expresion regular
+
+// /React/gi es una expresion regular que busca la palabra React en un string
+// /React/ig
+// /React/ig.test('React') //true
+// /React/ig.test('react') //true
+// /React/ig.test('REACT') //true
+//test es un metodo de las expresiones regulares que devuelve true si la expresion regular coincide con el string
+
+
+
+console.log('Palabras:',wordCount);
+console.log('Palabras React: ',reactWordCount)
